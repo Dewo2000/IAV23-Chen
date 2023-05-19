116fa5c6eae9b919f57b46189f8f9bac987d25e4
Dewei Chen <deweiche@ucm.es>
2023-05-19 12:34:10 +0200

    Añadido observación y acción

--- My project/Assets/Script/MoveToPointAgent.cs
+++ My project/Assets/Script/MoveToPointAgent.cs
@@ -7,13 +7,18 @@ using Unity.MLAgents.Sensors;
 
 public class MoveToPointAgent : Agent
 {
+    [SerializeField] private Transform checkpointr;
+    public float speed;
     public override void OnActionReceived(ActionBuffers actions)
     {
-        
+        float x = actions.ContinuousActions[0];
+        float z = actions.ContinuousActions[1];
+        transform.position += new Vector3(x, 0, z) * Time.deltaTime * speed;
     }
     public override void CollectObservations(VectorSensor sensor)
     {
-        
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(checkpointr.position);
     }
     public override void OnEpisodeBegin()
     {
