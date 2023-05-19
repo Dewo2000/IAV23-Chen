acee20e907dbdcbb992035ca9462dd1851cddd8c
Dewei Chen <deweiche@ucm.es>
2023-05-19 07:24:23 +0200

    Add mlagent , escena principal , script agente

---
+++ My project/Assets/Script/MoveToPointAgent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToPointAgent : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+    }
+    public override void OnEpisodeBegin()
+    {
+        
+    }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+       
+    }
+}
